8e65b681ef383fd1e3a8dd832fb0df0410437010
unknown <facundocolavitte@gmail.com>
2022-12-03 10:25:09 -0300

    Ejercicio 4 en asm

--- Ejercicios/adm_c16/Core/Src/main.c
+++ Ejercicios/adm_c16/Core/Src/main.c
@@ -64,6 +64,7 @@ static void MX_USB_OTG_FS_PCD_Init(void);
 /* USER CODE BEGIN PFP */
 void asm_zeros (uint32_t * vector, uint32_t longitud);
 extern void asm_productoEscalar32 (uint32_t * vectorIn, uint32_t * vectorOut, uint32_t longitud, uint32_t escalar);
+extern void asm_productoEscalar12 (uint16_t * vectorIn, uint16_t * vectorOut, uint32_t longitud, uint16_t escalar);
 void filtroVentana10(uint16_t * vectorIn, uint16_t * vectorOut, uint32_t longitudVectorIn);
 /* USER CODE END PFP */
 
